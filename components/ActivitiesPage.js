73bddf063db063cebc7246431dbb12cf88cd851d
spartanj3t6 <129471049+spartanj3t6@users.noreply.github.com>
2023-04-06 20:12:27 +1000

    Fixed activities again

--- components/ActivitiesPage.js
+++ components/ActivitiesPage.js
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import activities from './Activities';
-import Activity from './Activity';
+import ActivityCard from './ActivityCard';
 
 function ActivitiesPage() {
   return (
     <div className="activities-page">
       <h1>Activities</h1>
       {activities.map((activity) => (
-        <Activity
+        <ActivityCard
           key={activity.id}
           name={activity.name}
           category={activity.category}
